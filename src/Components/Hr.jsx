d723f65a8476e4664df0709eda7a4a19f7b883ce
firoz.nur <firoznur5@gmail.com>
2024-11-08 19:15:23 +0600

    Fix : Horizontal Line For Small Device

--- src/Components/Hr.jsx
+++ src/Components/Hr.jsx
@@ -1,10 +1,6 @@
-import React from 'react';
-
 const Hr = () => {
   return (
-    <div className='my-20 border-t-2 border-dashed border-black'>
-
-    </div>
+    <div className="my-20 ml-4 mr-4 border-t-2 border-dashed border-black md:ml-0 md:mr-0"></div>
   );
 };
 
