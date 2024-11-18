a320735c4d6c655c2260194ae4f86cc989ca64aa
firoz.nur <firoznur5@gmail.com>
2024-11-18 13:56:20 +0600

    Add : Projects Card

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -38,11 +38,55 @@ const Projects = () => {
     },
   ];
 
+  const ProjectCard = ({
+    title,
+    image,
+    description,
+    clientLink,
+    clientGithub,
+    serverGithub,
+    technologies,
+  }) => {
+    return (
+      <div className="mx-4 flex h-full flex-col rounded border border-black p-6 shadow hover:shadow-2xl sm:mx-1">
+        <h3 className="mb-2 text-2xl font-semibold">{title}</h3>
+        <img
+          src={image}
+          alt={title}
+          className="mb-4 h-48 w-full rounded border border-black object-cover object-top shadow"
+        />
+        <p className="mb-2">{description}</p>
+        <div className="flex-grow"></div>
+        <hr className="border-dashed border-gray-300" />
+        <p>Technologies Used: {technologies}</p>
+        <hr className="my-2 border-dashed border-gray-300" />
+        <p className="font-semibold">
+          Client Side Link:{" "}
+          <a href={clientLink} className="text-blue-500 hover:underline">
+            Live Link
+          </a>{" "}
+          |{" "}
+          <a href={clientGithub} className="text-blue-500 hover:underline">
+            GitHub
+          </a>
+        </p>
+        <p className="font-semibold">
+          Server Side Link:{" "}
+          <a href={serverGithub} className="text-blue-500 hover:underline">
+            GitHub
+          </a>
+        </p>
+      </div>
+    );
+  };
+
+
+
   return (
     <section id="projects">
       <h2 className="mb-10 text-center text-4xl text-gray-800">Projects</h2>
       <div className="container mx-auto grid grid-cols-1 gap-10 md:grid-cols-2 lg:grid-cols-3">
-        <div className="mx-4 flex h-full flex-col rounded border border-black p-6 shadow hover:shadow-2xl sm:mx-1">
+        {/* <div className="mx-4 flex h-full flex-col rounded border border-black p-6 shadow hover:shadow-2xl sm:mx-1">
           <h3 className="mb-2 text-2xl font-semibold">1. Fit N Flex Arena</h3>
           <img
             src="https://i.ibb.co/jb9sd3J/Screenshot-2024-07-04-172855.png"
@@ -84,9 +128,9 @@ const Projects = () => {
               GitHub
             </a>
           </p>
-        </div>
+        </div> */}
 
-        <div className="mx-4 flex h-full flex-col rounded border border-black p-6 shadow hover:shadow-2xl sm:mx-1">
+        {/* <div className="mx-4 flex h-full flex-col rounded border border-black p-6 shadow hover:shadow-2xl sm:mx-1">
           <h3 className="mb-2 text-2xl font-semibold">2. Food Fare</h3>
           <img
             src="https://i.ibb.co/ZMp1FJT/Screenshot-2024-07-04-172925.png"
@@ -128,9 +172,9 @@ const Projects = () => {
               GitHub
             </a>
           </p>
-        </div>
+        </div> */}
 
-        <div className="mx-4 flex h-full flex-col rounded border border-black p-6 shadow hover:shadow-2xl sm:mx-1">
+        {/* <div className="mx-4 flex h-full flex-col rounded border border-black p-6 shadow hover:shadow-2xl sm:mx-1">
           <h3 className="mb-2 text-2xl font-semibold">3. Dream Tourism</h3>
           <img
             src="https://i.ibb.co/dJwqRMs/Screenshot-2024-07-04-173015.png"
@@ -171,10 +215,10 @@ const Projects = () => {
               GitHub
             </a>
           </p>
-        </div>
-        {projectsInfo.map((project) => {
-          console.log("test", project.title);
-        })}
+        </div> */}
+        {projectsInfo.map((project) => (
+          <ProjectCard key={project.title} {...project} />
+        ))}
       </div>
     </section>
   );
