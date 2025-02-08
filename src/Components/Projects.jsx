c7c07f8850607f0b7729a3931229b27472a60fd4
firoz.nur <firoznur5@gmail.com>
2025-02-08 12:02:09 +0600

    Add : Separate File For Project Card

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,3 +1,5 @@
+import ProjectCard from "./ProjectCard";
+
 const Projects = () => {
   const projectsInfo = [
     {
@@ -38,49 +40,47 @@ const Projects = () => {
     },
   ];
 
-  const ProjectCard = ({
-    title,
-    image,
-    description,
-    clientLink,
-    clientGithub,
-    serverGithub,
-    technologies,
-  }) => {
-    return (
-      <div className="mx-4 flex h-full flex-col rounded border border-black p-6 shadow hover:shadow-2xl sm:mx-1">
-        <h3 className="mb-2 text-2xl font-semibold">{title}</h3>
-        <img
-          src={image}
-          alt={title}
-          className="mb-4 h-48 w-full rounded border border-black object-cover object-top shadow"
-        />
-        <p className="mb-2">{description}</p>
-        <div className="flex-grow"></div>
-        <hr className="border-dashed border-gray-300" />
-        <p>Technologies Used: {technologies}</p>
-        <hr className="my-2 border-dashed border-gray-300" />
-        <p className="font-semibold">
-          Client Side Link:{" "}
-          <a href={clientLink} className="text-blue-500 hover:underline">
-            Live Link
-          </a>{" "}
-          |{" "}
-          <a href={clientGithub} className="text-blue-500 hover:underline">
-            GitHub
-          </a>
-        </p>
-        <p className="font-semibold">
-          Server Side Link:{" "}
-          <a href={serverGithub} className="text-blue-500 hover:underline">
-            GitHub
-          </a>
-        </p>
-      </div>
-    );
-  };
-
-
+  // const ProjectCard = ({
+  //   title,
+  //   image,
+  //   description,
+  //   clientLink,
+  //   clientGithub,
+  //   serverGithub,
+  //   technologies,
+  // }) => {
+  //   return (
+  //     <div className="mx-4 flex h-full flex-col rounded border border-black p-6 shadow hover:shadow-2xl sm:mx-1">
+  //       <h3 className="mb-2 text-2xl font-semibold">{title}</h3>
+  //       <img
+  //         src={image}
+  //         alt={title}
+  //         className="mb-4 h-48 w-full rounded border border-black object-cover object-top shadow"
+  //       />
+  //       <p className="mb-2">{description}</p>
+  //       <div className="flex-grow"></div>
+  //       <hr className="border-dashed border-gray-300" />
+  //       <p>Technologies Used: {technologies}</p>
+  //       <hr className="my-2 border-dashed border-gray-300" />
+  //       <p className="font-semibold">
+  //         Client Side Link:{" "}
+  //         <a href={clientLink} className="text-blue-500 hover:underline">
+  //           Live Link
+  //         </a>{" "}
+  //         |{" "}
+  //         <a href={clientGithub} className="text-blue-500 hover:underline">
+  //           GitHub
+  //         </a>
+  //       </p>
+  //       <p className="font-semibold">
+  //         Server Side Link:{" "}
+  //         <a href={serverGithub} className="text-blue-500 hover:underline">
+  //           GitHub
+  //         </a>
+  //       </p>
+  //     </div>
+  //   );
+  // };
 
   return (
     <section id="projects">
