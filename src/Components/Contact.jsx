6ce7558b9808f9e0267c71a839badc6314065af9
firoz.nur <firoznur5@gmail.com>
2025-04-01 10:46:19 +0600

    Add : Open Links In New Tab

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,4 +1,9 @@
-import { FaGithub, FaLinkedin, FaPhoneSquareAlt, FaWhatsappSquare } from "react-icons/fa";
+import {
+  FaGithub,
+  FaLinkedin,
+  FaPhoneSquareAlt,
+  FaWhatsappSquare,
+} from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
 import { FaFacebook } from "react-icons/fa";
 import AOS from "aos";
@@ -39,6 +44,7 @@ const Contact = () => {
               </a>
               <a
                 href="https://wa.me/8801797626050?text=Hi%20Md.%20Firoz%20Mahmud%20Nur,%20I%20came%20across%20your%20portfolio%20website%20and%20would%20like%20to%20connect%20with%20you."
+                target="_blank"
                 className="flex items-center text-lg"
                 data-aos="fade-up"
                 data-aos-duration="500"
@@ -48,6 +54,7 @@ const Contact = () => {
               </a>
               <a
                 href="https://www.linkedin.com/in/md-firoz-mahmud-nur"
+                target="_blank"
                 className="flex items-center text-lg"
                 data-aos="fade-up"
                 data-aos-duration="500"
@@ -57,6 +64,7 @@ const Contact = () => {
               </a>
               <a
                 href="https://github.com/Md-Firoz-Mahmud-Nur"
+                target="_blank"
                 className="flex items-center text-lg"
                 data-aos="fade-up"
                 data-aos-duration="500"
@@ -66,6 +74,7 @@ const Contact = () => {
               </a>
               <a
                 href="mailto:firoznur5@gmail.com"
+                target="_blank"
                 className="flex items-center text-lg"
                 data-aos="fade-up"
                 data-aos-duration="500"
