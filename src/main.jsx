5fd905fce147761626fc0c40826fafdf129311ba
firoz.nur <firoznur5@gmail.com>
2025-07-19 23:49:08 +0600

    Add : Router & Error Page

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,45 @@ import "./index.css";
 import ParticlesAnimation from "./ParticlesAnimation/ParticlesAnimation.jsx";
 import CursorAnimation from "./CursorAnimation/CursorAnimation.jsx";
 import Provider from "./Context/Context.jsx";
+import Contact from "./Components/Contact.jsx";
+import Root from "./Root.jsx";
+import ErrorPage from "./ErrorPage.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Projects from "./Components/Projects.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: "/",
+        element: <App></App>,
+      },
+      {
+        path: "/projects",
+        element: (
+          <div className="py-10">
+            <Projects></Projects>
+          </div>
+        ),
+      },
+      {
+        path: "/contact",
+        element: <Contact></Contact>,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider>
       <ParticlesAnimation></ParticlesAnimation>
       <CursorAnimation></CursorAnimation>
-      <App />
+      {/* <App /> */}
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>,
 );
