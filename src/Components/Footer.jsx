a6519e5fc2cf85abe4e7255cc8be2a522a335df1
firoz.nur <firoznur5@gmail.com>
2025-07-25 00:57:33 +0600

    Add : Dependency

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -25,7 +25,7 @@ const Footer = () => {
                     behavior: "smooth",
                   });
                 }, 1);
-                
+
                 // e.preventDefault();
                 // window.scrollTo({
                 //   top: document.getElementById(section).offsetTop - 145,
@@ -39,7 +39,7 @@ const Footer = () => {
         ))}
       </>
     ),
-    [],
+    [navigate],
   );
 
   return (
