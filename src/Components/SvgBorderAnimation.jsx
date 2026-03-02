0ad857b2fbbeddbe88541d6f32fef44942ce6c94
firoz.nur <firoznur5@gmail.com>
2026-03-02 11:02:56 +0600

    Add : Header Download

--- src/Components/SvgBorderAnimation.jsx
+++ src/Components/SvgBorderAnimation.jsx
@@ -6,23 +6,34 @@ const SvgBorderAnimation = () => {
   const { isDark } = useContext(Context);
 
   return (
-    <button className="Button mt-1">
-      <svg className="Button-svg" width="200" height="50" viewBox="0 0 300 80">
-        <rect
-          className="Button-line--outer"
-          strokeWidth="3"
-          stroke={isDark === "dark" ? "#fff" : "#000"}
-          strokeLinecap="round"
-          fill="none"
-          x="-35"
-          y="1"
-          width="calc(100% + 66px)"
-          height="calc(100% - 2px)"
-          rx="5"
-        />
-      </svg>
-      <div className="Button-content">Download Resume</div>
-    </button>
+    <a
+      href="/Resume of Md Firoz Mahmud Nur.pdf"
+      download="Resume of Md Firoz Mahmud Nur.pdf"
+      className="inline-block"
+    >
+      <button className="Button mt-1">
+        <svg
+          className="Button-svg"
+          width="200"
+          height="50"
+          viewBox="0 0 300 80"
+        >
+          <rect
+            className="Button-line--outer"
+            strokeWidth="3"
+            stroke={isDark === "dark" ? "#fff" : "#000"}
+            strokeLinecap="round"
+            fill="none"
+            x="-35"
+            y="1"
+            width="calc(100% + 66px)"
+            height="calc(100% - 2px)"
+            rx="5"
+          />
+        </svg>
+        <div className="Button-content">Download Resume</div>
+      </button>
+    </a>
   );
 };
 
